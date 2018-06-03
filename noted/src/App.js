d28c9a76958affe9484cd6f5ca0bbee0e29f32bd
eddichen <contacteddichen@gmail.com>
2018-06-03 14:41:19 +0100

    adding an add not form and storage with firebase

--- noted/src/App.js
+++ noted/src/App.js
@@ -1,18 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import base from "./base";
 
 class App extends Component {
+  state = {
+    notes: {}
+  };
+
+  componentDidMount() {
+    this.refNotes = base.syncState(`notes`, {
+      context: this,
+      state: "notes"
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.refNotes);
+  }
+
+  addNote = e => {
+    e.preventDefault();
+    const createdAt = Date.now();
+    const title = this.noteTitle.value;
+    const content = this.noteContent.value;
+    const notes = { ...this.state.notes };
+
+    notes[createdAt] = {
+      createdAt,
+      title,
+      content
+    };
+    this.setState({ notes });
+    e.currentTarget.reset();
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form method="post" onSubmit={this.addNote}>
+          <label htmlFor="noteTitle">Title</label>
+          <input
+            type="text"
+            id="noteTitle"
+            name="noteTitle"
+            ref={noteTitle => {
+              this.noteTitle = noteTitle;
+            }}
+          />
+          <label htmlFor="noteContent">Note</label>
+          <textarea
+            id="noteContent"
+            name="noteContent"
+            ref={noteContent => {
+              this.noteContent = noteContent;
+            }}
+          />
+          <button type="submit">Add Note</button>
+        </form>
       </div>
     );
   }
